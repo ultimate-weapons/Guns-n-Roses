28e96c52340d51b320b28eb895ac54f6fe5cf317
gharsalliyasser <gharsalliyasser009@gmail.com>
2020-10-24 09:40:04 +0100

    all done

--- script.js
+++ script.js
@@ -54,21 +54,6 @@ function search(){
      
 }
 
-
-function clickswitch(str){
-    cataloge.innerHTML = "";
-    var result = allProducts.filter(function(element){
-        var test = "";
-        for (const key in element) {
-            test += element[key];
-        }
-        test = test.toLowerCase();
-        return (test.includes(str));
-    }); 
-    generateData(result);
-}
-
-
 const searchBtn = document.getElementById("search_btn");
 
 function searchclick(){
@@ -77,30 +62,64 @@ function searchclick(){
 searchclick();
 
 
+function takeId(theIdHolder){
+    return parseInt(theIdHolder.attributes[1].value);
+}
+
+
 // finish the click-on function like the food one and thats all;
 const closePopup = document.querySelector("#closer");
 const popupGun = document.querySelector("#pop_gun");
 
 function closePopupClick(){
     closePopup.addEventListener('click', function(){
+    
         popupGun.classList.add('close');
     })
 }
+
 closePopupClick();
 
+const byuBtnWpn = document.querySelector('#buy_btn');
+const deletebtnWpn = document.querySelector('#delete_btn');
+var sumBuy = 0;
 
-function takeId(theIdHolder){
-    return parseInt(theIdHolder.attributes[1].value);
+function buyGun(){
+    byuBtnWpn.addEventListener('click', function(){
+        var price = document.querySelector('#pop_price').innerText;
+        sumBuy += parseInt(price);
+        var checker = document.querySelector('.product_spec');
+        allProducts[takeId(checker)].both++;
+        document.querySelector('#unit_both').innerText = allProducts[takeId(checker)].both;
+        document.querySelector('#cost').innerText = sumBuy +" $";
+    })
 }
-
+function deleteGun(){
+    deletebtnWpn.addEventListener('click', function(){
+        var price = document.querySelector('#pop_price').innerText;
+        var checkid = document.querySelector('.product_spec');
+        var checkboth = document.querySelector('#unit_both').innerText;
+        checkerboth = parseInt(checkboth);
+        if(sumBuy > 0 && checkerboth > 0){
+            sumBuy -= parseInt(price);
+            allProducts[takeId(checkid)].both--;
+            document.querySelector('#unit_both').innerText = allProducts[takeId(checkid)].both;
+            document.querySelector('#cost').innerText = sumBuy +" $";
+        }
+    })
+}
+buyGun();
+deleteGun();
 
 
 function showGun(clicker){ 
     clicker.addEventListener('click', function(){
         popupGun.classList.remove('close');
         var applay = {...allProducts[takeId(clicker)]};
+        document.querySelector('.product_spec').setAttribute('id', applay.idg);
         document.querySelector("#pop_text").innerText = applay.name;
         document.querySelector("#pop_price").innerText = applay.price;
+        document.querySelector("#unit_both").innerText = applay.both;
         document.querySelector("#pop_div").style.background = "url("+ applay.imgLarge +") no-repeat";
         document.querySelector("#pop_div").style.backgroundSize = "contain";
         applay = {};
@@ -110,3 +129,18 @@ function showGun(clicker){
 const productSlots = document.querySelectorAll('.grid_product');
 
 productSlots.forEach(Element => showGun(Element));
+
+const cartBtnConf = document.querySelector('#cart_box_num');
+
+function confirmcart(){
+    cartBtnConf.addEventListener('click', function(){
+        alert('your tnasaction cost is : '+ sumBuy +'$\nTransaction have been saved and pending for aproval\nThank you for lock\'n loading with Us');
+        sumBuy = 0;
+        allProducts.forEach(function(element){
+            element.both = 0;
+        })
+        return location.reload();
+    })
+}
+
+confirmcart();
